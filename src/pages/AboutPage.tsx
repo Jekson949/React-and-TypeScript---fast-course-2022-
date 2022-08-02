c53db3dd296eda75503d4f856dccadab7b593a58
Jekson949 <jekson949@gmail.com>
2022-08-02 18:30:17 +0300

    first commit

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function AboutPage () {
+    return (
+        <div>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, numquam eos quo dolore reprehenderit debitis sit ullam facere sunt illo perferendis quis sed aperiam exercitationem molestias eveniet error quae, accusantium cumque quaerat adipisci? Possimus eveniet ipsam omnis rerum, commodi veniam pariatur nobis doloremque excepturi, incidunt natus quibusdam aspernatur maiores recusandae!
+        </div>
+    )
+}
